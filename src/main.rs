ca5b7a415d56deafc596d961467151fb0739c690
integraledelebesgue <jsmolka@student.agh.edu.pl>
2024-03-22 16:28:38 +0100

    Prediction map

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::error::Error;
+use linfa::dataset::Records;
 use linfa_datasets::iris;
 use plotly::ImageFormat;
+use rand::{self, Rng};
 
-use clusterization::{Data, cluster_map::cluster_map};
+#[allow(unused_imports)]
+use clusterization::{Data, cluster_map, prediction_map, scatter_matrix};
 
-const DEST: &'static str = "images/clusters.png";
+const DEST: &'static str = "images/prediction.png";
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let data: Data = iris();
 
-    // let plot = scatter_matrix(data, "Iris")?;
+    // let plot = scatter_matrix::plot(data, "Iris")?;
 
-    let plot = cluster_map(data, 0, 1, "Iris")?;
+    // let plot = cluster_map::plot(data, 0, 1, "Iris")?;
+
+    let n = data.nsamples();
+    let prediction = (0..n)
+        .map(|_| rand::thread_rng().gen_range(0..=2))
+        .collect::<Vec<_>>();
+
+    let plot = prediction_map::plot(
+        data,
+        prediction,
+        0,
+        1,
+        "Iris"
+    )?;
 
     plot.write_image(
         DEST,
---
+++ src/prediction_map.rs
@@ -0,0 +1,98 @@
+use std::error::Error;
+use ndarray::{s, Ix1};
+use plotly::{
+    color::NamedColor,
+    common::{Font, Title, Mode, Marker},
+    layout::{Axis, Legend}, 
+    Layout, Plot, Scatter
+};
+
+use crate::{Data, INDICATOR_COLORS, INDICATOR_LABELS};
+
+
+pub fn plot(
+    data: Data,
+    prediction: Vec<usize>,
+    x: usize,
+    y: usize,
+    title: &str
+) -> Result<Plot, Box<dyn Error>> {
+    let names = data.feature_names();
+
+    let x_name = &names[x];
+    let y_name = &names[y];
+
+    let records = data.records();
+
+    let x = records
+        .slice(s![.., x])
+        .into_dimensionality::<Ix1>()?
+        .to_vec();
+
+    let y = records
+        .slice(s![.., y])
+        .into_dimensionality::<Ix1>()?
+        .to_vec();
+
+    let indicator = data
+        .targets()
+        .iter()
+        .zip(prediction)
+        .map(|(e1, e2)| *e1 == e2);
+
+    let mut matching_x = Vec::<f64>::new();
+    let mut matching_y = Vec::<f64>::new();
+    let mut non_matching_x = Vec::<f64>::new();
+    let mut non_matching_y = Vec::<f64>::new();
+
+    indicator
+        .enumerate()
+        .for_each(|(i, val)| match val {
+            true => {
+                matching_x.push(x[i]);
+                matching_y.push(y[i]);
+            },
+            false => {
+                non_matching_x.push(x[i]);
+                non_matching_y.push(y[i]);
+            }
+        });
+
+    let layout = Layout::new()
+        .title(Title::new(title).font(Font::new().size(25)))
+        .x_axis(Axis::new().anchor("x").title(Title::new(x_name)))
+        .y_axis(Axis::new().anchor("y").title(Title::new(y_name)))
+        .legend(
+            Legend::new()
+                .title("Prediction".into())
+                .border_color(NamedColor::Gray)
+                .border_width(1)
+        );
+
+    let mut plot = Plot::new();
+    plot.set_layout(layout);
+
+    let matching = Scatter::new(matching_x, matching_y)
+        .mode(Mode::Markers)
+        .marker(
+            Marker::new()
+                .color(INDICATOR_COLORS[1])
+                .size(7)
+        )
+        .name(INDICATOR_LABELS[1]);
+
+    plot.add_trace(matching);
+
+    let non_matching = Scatter::new(non_matching_x, non_matching_y)
+        .mode(Mode::Markers)
+        .marker(
+            Marker::new()
+                .color(INDICATOR_COLORS[0])
+                .size(7)
+        )
+        .name(INDICATOR_LABELS[0]);
+
+    plot.add_trace(non_matching);
+    
+    Ok(plot)
+}
