f1d366bc8b3e1b676dde07dca7054ec97b628cd8
integraledelebesgue <jsmolka@student.agh.edu.pl>
2024-03-25 18:26:45 +0100

    Random solution generation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::error::Error;
+use clusterization::model::solution::{Discrete, Fuzzy};
+use itertools::Itertools;
 use linfa::dataset::Records;
 use linfa_datasets::iris;
 use plotly::ImageFormat;
-use rand::{self, Rng};
 
 #[allow(unused_imports)]
 use clusterization::{Data, plot::{cluster_map, prediction_map, scatter_matrix}};
@@ -17,14 +18,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // let plot = cluster_map::plot(data, 0, 1, "Iris")?;
 
-    let n = data.nsamples();
-    let prediction = (0..n)
-        .map(|_| rand::thread_rng().gen_range(0..=2))
-        .collect::<Vec<_>>();
+    let n_samples = data.nsamples();
+    let n_classes = data
+        .targets()
+        .iter()
+        .unique()
+        .collect::<Vec<_>>()
+        .len();
+    
+    let population = Fuzzy::random(n_samples, n_classes);
+    let prediction: Discrete = population.try_into()?;
 
     let plot = prediction_map::plot(
         data,
-        prediction,
+        prediction.to_vec(),
         0,
         1,
         "Iris"
--- src/model/solution.rs
+++ src/model/solution.rs
@@ -1,9 +1,22 @@
 use ndarray::{Array1, Array2, Axis};
 use ndarray_stats::{errors::MinMaxError, QuantileExt};
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Uniform;
+
 
 use crate::Data;
 
+#[derive(Debug)]
 pub struct Fuzzy(pub Array2<f64>);
+
+impl Fuzzy {
+    pub fn random(n_samples: usize, n_classes: usize) -> Self {
+        Fuzzy(Array2::random(
+            (n_samples, n_classes),
+            Uniform::new(0.0, 1.0)
+        ))
+    }
+}
 
 impl TryInto<Probabilistic> for Fuzzy {
     type Error = MinMaxError;
@@ -21,6 +34,16 @@ impl TryInto<Probabilistic> for Fuzzy {
     }
 }
 
+impl TryInto<Discrete> for Fuzzy {
+    type Error = MinMaxError;
+
+    fn try_into(self) -> Result<Discrete, Self::Error> {
+        let prob: Probabilistic = self.try_into()?;
+        prob.try_into()
+    }
+}
+
+#[derive(Debug)]
 pub struct Probabilistic(pub Array2<f64>);
 
 impl TryInto<Discrete> for Probabilistic {
@@ -28,12 +51,13 @@ impl TryInto<Discrete> for Probabilistic {
 
     fn try_into(self) -> Result<Discrete, Self::Error> {
         Ok(Discrete(self.0.map_axis(
-            Axis(0),
+            Axis(1),
             |row| row.argmax().unwrap()
         )))
     }
 }
 
+#[derive(Debug)]
 pub struct Discrete(pub Array1<usize>);
 
 impl Discrete {
@@ -43,5 +67,15 @@ impl Discrete {
             .to_owned();
 
         Discrete(target)
+    }
+
+    pub fn to_vec(self) -> Vec<usize> {
+        self.0.to_vec()
+    }
+}
+
+impl Into<Vec<usize>> for Discrete {
+    fn into(self) -> Vec<usize> {
+        self.to_vec()
     }
 }
