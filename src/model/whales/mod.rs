fcac3573aec36d369e5b15f37db91f36e52401a9
Krzysztof Pęczek <peczek.kr@gmail.com>
2024-05-06 00:22:51 +0200

    Implementing equation (1)

--- src/model/whales/mod.rs
+++ src/model/whales/mod.rs
@@ -1,56 +1,73 @@
-use super::solution::Fuzzy;
-use crate::Data;
-use rand::Rng;
+pub struct GSAParameters {}
 
-use ndarray::Array2;
-use ndarray_rand::rand_distr::Uniform;
-use ndarray_rand::RandomExt;
-use std::error::Error;
+pub fn fit(data: &Data, params: GSAParameters) -> Result<Fuzzy, Box<dyn Error>> {
+    use super::solution::Fuzzy;
+    use crate::Data;
+    use rand::Rng;
 
-pub struct WOAParameters {
-    pub n_classes: usize,
-    pub agents_total: usize,
-    pub max_iterations: usize,
-    pub spiral_constant: f64,
-}
+    use ndarray::Array2;
+    use ndarray_rand::rand_distr::Uniform;
+    use ndarray_rand::RandomExt;
+    use std::error::Error;
 
-fn agents_fitness(agents: &Vec<Fuzzy>, data: &Data) -> Vec<f64> {
-    agents
-        .iter()
-        .map(|agent| agent.fitness(&data))
-        .collect::<Vec<_>>()
-}
-
-pub fn fit(data: &Data, params: WOAParameters) -> Result<Fuzzy, Box<dyn Error>> {
-    let n_samples = data.records.nrows();
-
-    let mut agents: Vec<Fuzzy> = (0..params.agents_total)
-        .map(|_| Fuzzy::random(n_samples, params.n_classes))
-        .collect();
-
-    let mut rng = rand::thread_rng();
-
-    for time in 1..=params.max_iterations {
-        let a = 2.0 * params.max_iterations as f64 / time as f64;
-        let r_1 = Array2::random((n_samples, params.n_classes), Uniform::new(0.0, 1.0));
-        let r_2 = Array2::random((n_samples, params.n_classes), Uniform::new(0.0, 1.0));
-
-        let A: Array2<f64> = a * (2.0 * &r_1 - 1.0);
-        let C: Array2<f64> = 2.0 * &r_2;
-
-        for agent in &agents {
-            if rng.gen_range(0.0..1.0) > 0.5 {
-                if (&A * &A).sum() < 1.0 {
-                    todo!("Update the position of the current search agent by the equation (1)");
-                } else {
-                    todo!("Select a random search agent (X_rand)
-                                Update the position of the current search agent by the equation (3)");
-                }
-            } else {
-                todo!("");
-            }
-        }
+    pub struct WOAParameters {
+        pub n_classes: usize,
+        pub agents_total: usize,
+        pub max_iterations: usize,
+        pub spiral_constant: f64,
     }
 
-    todo!("I was lazy");
+    fn best_agent_index(agents: &Vec<Fuzzy>, data: &Data) -> usize {
+        let mut best_index = 0;
+        let mut best_fitness = f64::NEG_INFINITY;
+        for (i, agent) in agents.iter().enumerate() {
+            let fitness = agent.fitness(data);
+            best_index = if fitness > best_fitness {
+                best_fitness = fitness;
+                i
+            } else {
+                best_index
+            }
+        }
+        best_index
+    }
+
+    pub fn fit(data: &Data, params: WOAParameters) -> Result<Fuzzy, Box<dyn Error>> {
+        let n_samples = data.records.nrows();
+
+        let mut agents: Vec<Fuzzy> = (0..params.agents_total)
+            .map(|_| Fuzzy::random(n_samples, params.n_classes))
+            .collect();
+
+        let mut rng = rand::thread_rng();
+
+        for time in 1..=params.max_iterations {
+            let a = 2.0 * params.max_iterations as f64 / time as f64;
+            let r_1 = Array2::random((n_samples, params.n_classes), Uniform::new(0.0, 1.0));
+            let r_2 = Array2::random((n_samples, params.n_classes), Uniform::new(0.0, 1.0));
+
+            let A: Array2<f64> = a * (2.0 * &r_1 - 1.0);
+            let C: Array2<f64> = 2.0 * &r_2;
+
+            let fitnesses = agents_fitness(&agents, data);
+            let best_agent_index = best_agent_index(&fitnesses);
+            let best_agent = agents[best_agent_index];
+
+            for agent in &agents {
+                if rng.gen_range(0.0..1.0) > 0.5 {
+                    if (&A * &A).sum() < 1.0 {
+                        let Dupa = &C * &best_agent.distribution - &agent.distribution;
+                        // agent.distribution = &best_agent.distribution - &A * &Dupa;
+                    } else {
+                        todo!("Select a random search agent (X_rand)
+                                Update the position of the current search agent by the equation (3)");
+                    }
+                } else {
+                    todo!("");
+                }
+            }
+        }
+
+        todo!("I was lazy");
+    }
 }
