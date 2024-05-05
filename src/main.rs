ffb1213b965b3354301e4ef5c6d32a9ffe4064f0
Krzysztof Pęczek <peczek.kr@gmail.com>
2024-05-05 23:43:39 +0200

    Initial code for the WO algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::error::Error;
-use clusterization::model::{kmeans, metric, solution::{self, Discrete}};
+use clusterization::model::{
+    kmeans, metric,
+    solution::{self, Discrete},
+};
 use linfa_datasets::iris;
 use plotly::ImageFormat;
+use std::error::Error;
 
 #[allow(unused_imports)]
 use clusterization::{
-    Data,
+    model::gravity::{self, GSAParameters},
     plot::{cluster_map, prediction_map, scatter_matrix},
-    model::gravity::{self, GSAParameters}
+    Data,
 };
 
 const DEST: &'static str = "images/prediction.png";
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     let data: Data = iris();
@@ -19,14 +21,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     // let plot = scatter_matrix::plot(data, "Iris")?;
 
     // let plot = cluster_map::plot(data, 0, 1, "Iris")?;
-    
+
     // let solution = Fuzzy::random(n_samples, n_classes);
     let params = GSAParameters {
         n_classes: 3,
         agents_total: 10,
         max_iterations: 50,
         initial_gravity: 10.0,
-        gravity_decay: 0.01
+        gravity_decay: 0.01,
     };
 
     // let solution = gravity::fit(&data, params)?;
@@ -44,21 +46,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let accuracy = 100.0 * metric::accuracy(&truth, &prediction).unwrap();
     println!("Accuracy: {accuracy} %");
 
-    let plot = prediction_map::plot(
-        data,
-        prediction.to_vec(),
-        0,
-        1,
-        "Iris"
-    )?;
+    let plot = prediction_map::plot(data, prediction.to_vec(), 0, 1, "Iris")?;
 
-    plot.write_image(
-        DEST,
-        ImageFormat::PNG,
-        640,
-        420,
-        1.0
-    );
+    plot.write_image(DEST, ImageFormat::PNG, 640, 420, 1.0);
 
     Ok(())
 }
